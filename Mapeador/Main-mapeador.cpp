5dfe45c857e7964915c81fca493d2541b33d2cb1
echedey-luis-alvarez <80125792+echedey-luis-alvarez@users.noreply.github.com>
2021-06-18 17:14:17 +0200

    Substitute messy robot structs and funcs with functions that work on the robot, delete map, change keyboard input keys
    Just a big sweeping to the project

--- Mapeador/Main-mapeador.cpp
+++ Mapeador/Main-mapeador.cpp
@@ -30,43 +30,34 @@
 #define MOV_haciaIzq true
 #define MOV_haciaDer false
 
+// Alternatives
+#define MOVE_forward (char)1
+#define MOVE_backward (char)2
+
+#define MOVE_left (char)3
+#define MOVE_right (char)4
+
+// Keys numbers
+#ifndef KeyBrdLIB
+#define KeyBrdLIB
+
+#define Key_TAB 9
+#define Key_ENTER 13
+#define Key_ESC 27
+#define Arrow_UP 72
+#define Arrow_LEFT 75
+#define Arrow_RIGHT 77
+#define Arrow_DOWN 80
+
+#endif // !Key numbers
+
 // File handling definitions
 #define NAME_SZ 50
 // #define WP_PATH "WAYPOINTS" // Unused: in case files are saved into specific folder, use this folder name
 
 // Math definitions
 #define M_PI 3.14159265358979323846 // Pi
-#define DEF_PRECISION 5 // Default precision to use when printing vectors
-
-// Robot definitions
-
-#define WHEEL_RADIUS 1   // (1 is just an example) (m)
-#define ROBOT_WIDTH 1     // (1 is just an example) (m)
-#define WHEEL_TURN 1     //number of turns the wheel makes for each pulsation (forward and backward) (no units)
-#define ROT_ANGLE M_PI/2  // angle that the robot rotates for each pulsation (right and left) (rad)
-#define ANGULAR_VELOCITY 1  // angular velocity of wheel rotation (1 is just an example) (rad/s)
-
-// stuctures with variables necessary for robot movement
-
-typedef struct {
-
-    int forward, backwards, right, left;
-    int n_counter, s_counter, e_counter, o_counter;
-
-}pulsation;
-
-typedef struct {
-
-    double rotation, distance, displacement;
-    vector2D coords; // It contains the coordinates of the robot
-
-}coordinate;
-
-typedef struct {
-
-    double forward, backwards, rigth, left;
-
-}tiempos;
+#define DEF_PRECISION 5 // Default precision to use when printing decimals
 
 //// Program functions ////
 
@@ -89,13 +80,10 @@ int command_Arduino_time_s(char* buffer, size_t sz, bool tipoMovimiento, bool se
     */
 
 // GUI functions
-void DrawMap(coordinate*);
 void clearScreen(int numberofCharactersYouWantToClearInConsole); // Self-explanatory
 
 // Robot functions
-int ProcessPulsation(pulsation*, int);
-void Calc_Coordinate_Time(pulsation*, coordinate*, tiempos*);
-int comandoArduino_wrapper(char* Buffer, size_t sz, tiempos* mov); // Intermediario estructura de tiempos a comando de tiempo
+int rb_moveByDefs(robot* rb, char move, char* buffer, size_t sz, unsigned int* calculatedDuration);
 
 int main() // Main function
 {
@@ -111,18 +99,6 @@ int main() // Main function
     FILE* fp_puntos = NULL;
     char* nombreArchivoPuntos = (char*)calloc(NAME_SZ, sizeof(char));
 
-    // Initialize robot properties variables
-    // Declarate and initialize variable type pulsation
-    pulsation *p_pulsation1 = (pulsation*)calloc(1, sizeof(pulsation));
-    if (!p_pulsation1) return 21;
-
-    // Declarate and initialize variable type coordinate
-    coordinate* p_coord1 = (coordinate*)calloc(1, sizeof(coordinate));
-    if (!p_coord1) return 22;
-
-    // Declarate and initialize variable type tiempos
-    tiempos* p_time1 = (tiempos*)calloc(1, sizeof(tiempos));
-    if (!p_time1) return 23;
     // Robot properties
     robot* rb = (robot*)calloc(1, sizeof(robot));
     if (!rb) return 20;
@@ -176,7 +152,7 @@ int main() // Main function
     }
 
     // Imprimir controles
-    printf("Controls:\nMove Forward: 'n'\nMove Backward: 's'\nRotate to the right : 'e'\nRotate to the left: 'o'\nPress any key to continue...\n");
+    printf("Controls:\nMove Forward and Backward with Up and Down arrows\nRotate with Left and Right arrows\nPress any key to continue...\n");
     
     // Esperamos pulsacion para continuar
     (void)_getch();
@@ -187,55 +163,60 @@ int main() // Main function
         clearScreen(70);
     }
 
-    //Para dibujar los ejes cartesianos del mapa:
-    int xc = 50, yc = 50;
-
-    for (int i = 0; i < 120; i++) {
-        gotoxy(i, yc);
-        putchar(196);
-    }
-    for (int i = 0; i < 60; i++) {
-        gotoxy(xc, i + 20);
-        putchar('|');
-    }
-    gotoxy(xc, yc);
-    putchar('O');
-    gotoxy(0, 9);
-    // !Dibujar ejes
-
-    // Dibujar posici�n actual del robot y datos superiores
-    Calc_Coordinate_Time(p_pulsation1, p_coord1, p_time1);
 
     // Procesamiento de todo el algoritmo: entrada, procesamiento, salida en bucle
-    while ( Arduino->IsConnected() ) // Mientras el Arduino est� conectado se puede proceder
+    while ( Arduino->IsConnected() and status != 255) // Mientras el Arduino est� conectado se puede proceder
     {
         // Entrada de datos - pulsaciones del teclado
-        p_pulsation1->forward = 0;
-        p_pulsation1->backwards = 0;
-        p_pulsation1->right = 0;
-        p_pulsation1->left = 0;
+        puls = _getch();
 
-        // Entrada de datos - pulsaciones del teclado
-        ProcessPulsation(p_pulsation1, _getch());
+        switch (puls) {
+        case EOF:
+        case Key_ESC:
+            gotoxy(0, 0);
+            printf("Exiting program...");
+            Arduino->~Serial();
+            status = 255;
+            break;
 
-        //Computo coordenadas de salida y tiempos de movimientos
-        Calc_Coordinate_Time(p_pulsation1, p_coord1, p_time1);
-        DrawMap(p_coord1);
+        case 224: // Special key
+            switch (_getch())
+            {
+            case Arrow_UP:
+                status = MOVE_forward;
+                break;
+            case Arrow_DOWN:
+                status = MOVE_backward;
+                break;
+            case Arrow_LEFT:
+                status = MOVE_left;
+                break;
+            case Arrow_RIGHT:
+                status = MOVE_right;
+                break;
+            default:
+                break;
+            }
+            break;
 
-        // Imprimir el num de veces que se presionan las teclas
-        gotoxy(0, 1);
-        printf("Key counters: 'n': %d; 's': %d; 'e': %d; 'o': %d", p_pulsation1->n_counter, p_pulsation1->s_counter, p_pulsation1->e_counter, p_pulsation1->o_counter);
+        case 'n':
+        case 's':
+        case 'o':
+        case 'e':
+            gotoxy(0, 2);
+            printf("The use of the key '%c' is deprecated.", puls);
+        default:
+            status = 0;
+            break;
+        }
 
-        // Generar comando y si sale bien enviarlo
-        if (comandoArduino_wrapper(BufferSalida, BUF, p_time1) == 0) {
+        if (rb_moveByDefs(rb, status, BufferSalida, BUF, &millisWait) == 0) {
             // Enviar comando al robot
             Arduino->WriteData(BufferSalida, BUF);
 
             // Guardar vector posición
             waypts_bappend_vect(fp_puntos, &rb->position);
 
-            // Imprimir posición en el mapa de la pantalla
-            DrawMap(p_coord1);
             // Imprimir información por pantalla
             gotoxy(0, 0);
             robot_print_summary(rb, DEF_PRECISION);
@@ -298,135 +279,35 @@ int command_Arduino_time_s(char* buffer, size_t sz, bool tipoMovimiento, bool se
     return 0;
 }
 
-int comandoArduino_wrapper(char* Buffer, size_t sz, tiempos* mov) {
-    // Suponemos que tan solo un movimiento es no-cero
-    // N�tese la conversi�n a milisengundos que se hace
-    if (mov->forward != 0) {
-        return command_Arduino_time_s(Buffer, sz, MOV_lineal, MOV_haciaDelante, mov->forward * 1000);
+int rb_moveByDefs(robot* rb, char move, char* buffer, size_t sz, unsigned int *duration) {
+    if (move == MOVE_forward or move == MOVE_backward) {
+        vector2D movement = vector2D_productByScalar(vector2D_rotate({ 1, 0 }, rb->angle), rb->defMoves.linMove);
+        if (move == MOVE_forward) {
+            rb->position = vector2D_add(rb->position, movement);
+        }
+        else {
+            rb->position = vector2D_substract(rb->position, movement);
+        }
+
+        command_Arduino_time_s(buffer, sz, MOV_lineal, 
+            (move == MOVE_forward) ? MOV_haciaDelante : MOV_haciaAtras, (*duration = (unsigned int) rint(rb->defMoves.linMove / rb->physical.vel.lineal * 1000) ) );
     }
-    else if (mov->backwards != 0) {
-        return command_Arduino_time_s(Buffer, sz, MOV_lineal, MOV_haciaAtras, mov->backwards * 1000);
+    else if (move == MOVE_left or move == MOVE_right) {
+        if (move == MOVE_left) {
+            rb->angle += rb->defMoves.rotAngle;
+        }
+        else {
+            rb->angle -= rb->defMoves.rotAngle;
+        }
+        if (fabs(rb->angle) == 2 * M_PI) rb->angle = 0;
+
+        command_Arduino_time_s(buffer, sz, MOV_rotacion, 
+            (move == MOVE_left) ? MOV_haciaIzq : MOV_haciaDer, (*duration = (unsigned int) rint(rb->defMoves.rotAngle / rb->physical.vel.angular * 1000) ) );
     }
-    else if (mov->rigth != 0) {
-        return command_Arduino_time_s(Buffer, sz, MOV_rotacion, MOV_haciaDer, mov->rigth * 1000);
-    }
-    else if (mov->left != 0) {
-        return command_Arduino_time_s(Buffer, sz, MOV_rotacion, MOV_haciaIzq, mov->left * 1000);
-    }
-    else
-        *Buffer = '\0'; // En caso extraño se limpia la cadena
+    else {
         return -1;
-    return 0;
-}
-
-int ProcessPulsation(pulsation* p_pulsation, int inputChar) {
-
-    if (inputChar == EOF or inputChar == '\n') return 1;
-    switch (inputChar) {
-    case 'n':
-        p_pulsation->forward = 1;
-        p_pulsation->n_counter++;
-        break;
-    case 's':
-        p_pulsation->backwards = 1;
-        p_pulsation->s_counter++;
-        break;
-    case 'e':
-        p_pulsation->right = 1;
-        p_pulsation->e_counter++;
-        break;
-    case 'o':
-        p_pulsation->left = 1;
-        p_pulsation->o_counter++;
-        break;
-    default:
-        return 0;
     }
     return 0;
-}
-
-void Calc_Coordinate_Time(pulsation* p_pulsation, coordinate* p_coord, tiempos* p_time) {
-
-    // FORMULAS USED: //
-    // { S = wheel_radius * angular_velocity * time_forward.
-    // { S = rotation * (robot_width / 2). 
-    // { rotation = 2 * (wheel_radius / robot_width) * angular_velocity * time_right.
-
-    p_time->forward = 0.0;
-    p_time->backwards = 0.0;
-    p_time->rigth = 0.0;
-    p_time->left = 0.0;
-
-
-    if (p_pulsation->forward == 1) {
-
-        p_coord->displacement = (2 * M_PI * WHEEL_RADIUS) * WHEEL_TURN;
-
-        vector2D desplazamiento = { (p_coord->displacement) * sin(p_coord->rotation) , (p_coord->displacement) * cos(p_coord->rotation) };
-        
-        p_coord->coords = vector2D_add(p_coord->coords, desplazamiento);
-
-        p_time->forward = (p_coord->displacement) / (WHEEL_RADIUS * ANGULAR_VELOCITY);
-
-    }
-    else if (p_pulsation->backwards == 1) {
-
-        p_coord->displacement = (2 * M_PI * WHEEL_RADIUS) * WHEEL_TURN;
-
-        vector2D desplazamiento = { (p_coord->displacement) * sin(p_coord->rotation) , (p_coord->displacement) * cos(p_coord->rotation) };
-
-        p_coord->coords = vector2D_substract(p_coord->coords, desplazamiento);
-
-        p_time->backwards = (p_coord->displacement) / (WHEEL_RADIUS * ANGULAR_VELOCITY);
-
-    }
-
-    else if (p_pulsation->right == 1) {
-
-        p_coord->rotation += ROT_ANGLE;
-
-        p_time->rigth = (ROT_ANGLE * ROBOT_WIDTH) / (2 * WHEEL_RADIUS * ANGULAR_VELOCITY);
-
-    }
-    else if (p_pulsation->left == 1) {
-
-        p_coord->rotation -= ROT_ANGLE;
-
-        p_time->left = (ROT_ANGLE * ROBOT_WIDTH) / (2 * WHEEL_RADIUS * ANGULAR_VELOCITY);
-
-    }
-
-
-    p_coord->distance += p_coord->displacement;
-
-    //reset value of variables to 0
-    p_pulsation->forward = 0;
-    p_pulsation->backwards = 0;
-    p_pulsation->right = 0;
-    p_pulsation->left = 0;
-    p_coord->displacement = 0.0;
-
-    //show coordinates and other data
-    gotoxy(0, 0);
-    printf("Robot @ (x, y) = (%.5lf, %.5lf); Total displacement =  %.5lf; Distance from origin = %.5lf", p_coord->coords.x, p_coord->coords.y, p_coord->distance, vector2D_modulo(p_coord->coords) );
-
-    return;
-}
-
-void DrawMap(coordinate* p_coord) {
-    int xc = 50, yc = 50;
-    double corrector = 2 / (M_PI * WHEEL_RADIUS); //Para que las coordenadas mostradas en el mapa no sean m�ltiplo de PI (sino de un valor entero), ni dependan del radio
-    gotoxy((short)round(fabs((xc + 2 * corrector * p_coord->coords.x))), (short)round(fabs((yc - corrector * p_coord->coords.y))));
-    putchar('#');
-
-    // Ahora calculamos donde va el siguiente caracter que indica la rotaci�n
-    short pointerX, pointerY;
-    pointerX = (short)round(fabs((xc + 2 * corrector * p_coord->coords.x + copysign(round(fabs(sin(p_coord->rotation))), sin(p_coord->rotation)))));
-    pointerY = (short)round(fabs((yc - corrector * p_coord->coords.y + copysign(round(fabs(cos(p_coord->rotation))), -cos(p_coord->rotation)))));
-    gotoxy(pointerX, pointerY);
-    putchar('+');
-
-    return;
 }
 
 void clearScreen(int spaces) {
